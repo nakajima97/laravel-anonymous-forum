7c3ce6fe1e087e3aa24eadefc44a9df8d5aa2319
Yudai Nakajima <kituneudon97@gmail.com>
2022-02-05 18:51:11 +0900

    #5 トップ画面を最低限作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,29 @@
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
-                掲示板一覧画面
+<x-default-layout>
+    <x-slot name="header">
+        {{ __('掲示板') }}
+    </x-slot>
+
+    <div class="2xl:mx-auto 2xl:container w-full md:mx-24 md:px-12 px-4 my-4">
+        <div class="w-full flex items-center justify-center">
+            <div class="bg-gray-800 bg-opacity-80 md:py-16 w-full flex flex-col items-center justify-center z-40">
+                <h1 class="text-4xl font-semibold leading-9 text-white text-center">匿名掲示板へようこそ！！</h1>
+                <p class="text-base leading-normal text-center text-white mt-6">
+
+                </p>
             </div>
         </div>
+
+        <div>
+            <form action="get" class="flex justify-center">
+                <input type="text"
+                    class="grow shadow appearance-none border border-red-500 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                <input type="submit" value="検索"
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            </form>
+        </div>
+
+        <div>
+            掲示板一覧
+        </div>
     </div>
-</div>
+</x-default-layout>
