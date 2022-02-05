c926d1182abde95160ea590e8ab864acfd954422
Nakajima <kituneudon97@gmail.com>
2022-02-05 17:30:11 +0900

    migrationとmodelを作成
    * #2 migrationを作成
    
    * #2 model作成

---
+++ database/migrations/2022_02_05_163941_create_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forum_tags', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('forum_id');
+            $table->string('name', 40);
+            $table->timestamps();
+
+            $table->foreign('forum_id')->references('id')->on('forums');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
---
+++ database/migrations/2022_02_05_164555_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('forum_id');
+            $table->integer('comment_number');
+            $table->string('comment');
+            $table->string('handle_name');
+            $table->integer('reply_to')->nullable();
+            $table->timestamps();
+
+            $table->foreign('forum_id')->references('id')->on('forums');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
