82b32030369550a959220b8646a535fe81e4c571
nakajima97 <kituneudon97@gmail.com>
2022-02-27 12:16:28 +0900

    #28 スレッドごとにちゃんとレスができるよう修正

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -20,7 +20,7 @@ class ResponseController extends Controller
         $last_response_number = $thread->get_last_response_number();
 
         $response->content = $content;
-        $response->thread_id = $request->input('thread_id');
+        $response->thread_id = intval($request->input('thread_id'));
         $response->response_number = $last_response_number + 1;
         $response->handle_name = "";
         $response->save();
