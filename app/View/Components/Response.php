0434e22600f373ca3069d49ffd09782c62ab0189
nakajima97 <kituneudon97@gmail.com>
2022-03-09 09:14:01 +0900

    #27 ハンドルネームを設定&表示できるようにした

--- app/View/Components/Response.php
+++ app/View/Components/Response.php
@@ -8,16 +8,23 @@ class Response extends Component
 {
     public $number;
     public $content;
+    public $name;
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($number = 0, $content = "")
+    public function __construct($number = 0, $content = "", $name="")
     {
         $this->number = $number;
         $this->content = $content;
+
+        if (empty($name)) {
+            $name = "ななし";
+        }
+
+        $this->name = $name;
     }
 
     /**
--- resources/views/components/response.blade.php
+++ resources/views/components/response.blade.php
@@ -1,4 +1,5 @@
 <div class="mb-2">
     <p>{{ $number }}</p>
+    <p>{{ $name }}</p>
     <p>{{ $content }}</p>
 </div>
